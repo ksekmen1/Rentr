53a7ab21767587515b1c610e5f3f6caff15b2295
ksekmen1 <kaya.sekmen@gmail.com>
2023-04-06 17:54:22 +0200

    Forbundet med api

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
+const baseUrl = "https://rentrapi20230406164054.azurewebsites.net/api/Rentritem"
 Vue.createApp({
     data() {
         return {
+            RentrItem:[],
+            addData:{name:"", price:0, quantity:0},
+            addMessage: "",
+            nameToGetBy:"",
             
         }
     },
     methods: {
+        GetAllItems(){
+            this.helperGetAndShow(baseUrl)
+        },
+
+        GetByName(name){
+            const url = baseUrl +"?name=" + name
+            this.helperGetAndShow(url)
+
+
+        },
+
+        async helperGetAndShow(url){
+            try{
+                const response = await axios.get(url)
+                this.RentrItem = await response.data
+            } catch(ex){
+                alert(ex.message)
+            }
+        }
         
     },
 }).mount("#app")
